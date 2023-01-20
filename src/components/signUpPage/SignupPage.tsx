1c1d9febcf0a7ac5f46a4979a381a161039f4f1f
Hassane <hsisane5@gmail.com>
2023-01-20 22:14:22 +0100

    signup page

---
+++ src/components/signUpPage/SignupPage.tsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+
+const SignupPage = () => {
+  return (
+    <div>
+      <Header></Header>
+      <button>Sign up</button>
+    </div>
+  );
+};
+export default SignupPage;
