f8db2f7c7ae3f9ebaca0218640ce086adeb18bef
Hassane <hsisane5@gmail.com>
2023-02-12 19:38:45 +0100

    added conditional routing based on authentication

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,22 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
+import { useEffect, useState } from "react";
 import "./App.css";
 import LoginPage from "./components/loginPage/LoginPage";
 import SignupPage from "./components/signUpPage/SignupPage";
+import Header from "./components/signUpPage/Header";
 function App() {
+  const [userID, setUserID] = useState<string | null>(null);
+  useEffect(() => {
+    const localUserID = localStorage.getItem("UserID");
+    if (localUserID) setUserID(localUserID);
+  }, []);
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />}></Route>
+        {userID && <Route path="/" element={<Header />}></Route>}
+        {!userID && <Route path="/" element={<Navigate to="/login" />}></Route>}
         <Route path="/login" element={<LoginPage />}></Route>
         <Route path="/sign-up" element={<SignupPage />}></Route>
       </Routes>
