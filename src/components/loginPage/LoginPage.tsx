5eb2f1dd3f5d689eba5afc72b680e8bed31ba092
Hassane <hsisane5@gmail.com>
2023-02-13 19:53:05 +0100

    added login functionality

--- src/components/loginPage/LoginPage.tsx
+++ src/components/loginPage/LoginPage.tsx
@@ -1,10 +1,29 @@
 import WelcomeMessage from "./WelcomeMessage";
 import LoginForm from "./LoginForm";
 import { useState } from "react";
+import { db } from "../../firebase/firebase";
+import { collection, DocumentData, getDocs } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
   const [passwordValue, setPasswordValue] = useState("");
   const [emailValue, setEmailValue] = useState("");
+  const navigate = useNavigate();
+
+  const checkInputValuesAgainstDB = async (
+    e: React.FormEvent<HTMLButtonElement>
+  ) => {
+    e.preventDefault();
+    const usersGottenFromDB: DocumentData[] = [];
+    const users = await getDocs(collection(db, "users"));
+    users.forEach((user) => usersGottenFromDB.push(user.data()));
+    usersGottenFromDB.forEach((user) => {
+      if (emailValue === user.firstName && passwordValue === user.password) {
+        navigate("/home");
+        localStorage.setItem("UserID", user.userID);
+      } else return;
+    });
+  };
 
   return (
     <>
@@ -20,6 +39,7 @@ const LoginPage = () => {
           const element = e.target as HTMLInputElement;
           setPasswordValue(element.value);
         }}
+        loginOnClickHandler={checkInputValuesAgainstDB}
       ></LoginForm>
     </>
   );
