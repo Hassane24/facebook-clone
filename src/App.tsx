116d97682fdcdc34bf14afcee2ac0d1049efeb9e
Hassane <hsisane5@gmail.com>
2023-02-13 17:34:52 +0100

    fixed a conditional routing bug

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,21 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
-import { useEffect, useState } from "react";
 import "./App.css";
 import LoginPage from "./components/loginPage/LoginPage";
 import SignupPage from "./components/signUpPage/SignupPage";
 import Header from "./components/signUpPage/Header";
 function App() {
-  const [userID, setUserID] = useState<string | null>(null);
-  useEffect(() => {
-    const localUserID = localStorage.getItem("UserID");
-    if (localUserID) setUserID(localUserID);
-  }, []);
+  const localUserID = localStorage.getItem("UserID");
   return (
     <Router>
       <Routes>
-        {userID && <Route path="/" element={<Header />}></Route>}
-        {!userID && <Route path="/" element={<Navigate to="/login" />}></Route>}
-        <Route path="/login" element={<LoginPage />}></Route>
-        <Route path="/sign-up" element={<SignupPage />}></Route>
+        <Route
+          path="/"
+          element={localUserID ? <Header /> : <Navigate to="login" />}
+        ></Route>
+        <Route path="login" element={<LoginPage />}></Route>
+        <Route path="sign-up" element={<SignupPage />}></Route>
       </Routes>
     </Router>
   );
