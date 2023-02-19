0c4427a2926ffc2f15adea11fe558a792518f20e
Hassane <hsisane5@gmail.com>
2023-02-19 21:01:35 +0100

    styling sign up form

--- src/components/signUpPage/SignupForm.tsx
+++ src/components/signUpPage/SignupForm.tsx
@@ -1,4 +1,5 @@
 import { FormProps } from "../loginPage/LoginForm";
+import styles from "../../styles/signupPage/signupForm.module.css";
 
 interface SignupFormProps extends FormProps {
   firstNameOnChangeHandler: (e: React.FormEvent<HTMLInputElement>) => void;
@@ -10,31 +11,36 @@ interface SignupFormProps extends FormProps {
 }
 
 const SignupForm = (props: SignupFormProps) => (
-  <form>
-    <div>
+  <form className={styles.form}>
+    <div className={styles.newAccountMessage}>
       <h2>Create a new account</h2>
       <div>It's quick and easy.</div>
     </div>
-    <div>
+    <div className={styles.inputsContainer}>
+      <div className={styles.firstNameSurnameContainer}>
+        <input
+          className={styles.inputs}
+          type="text"
+          name="first-name"
+          id="first-name"
+          placeholder="First name"
+          value={props.firstNameValue}
+          onChange={props.firstNameOnChangeHandler}
+          required
+        />
+        <input
+          className={styles.inputs}
+          type="text"
+          name="surname"
+          id="surname"
+          placeholder="Surname"
+          value={props.surnameValue}
+          onChange={props.surnameOnChangeHandler}
+          required
+        />
+      </div>
       <input
-        type="text"
-        name="first-name"
-        id="first-name"
-        placeholder="First name"
-        value={props.firstNameValue}
-        onChange={props.firstNameOnChangeHandler}
-        required
-      />
-      <input
-        type="text"
-        name="surname"
-        id="surname"
-        placeholder="Surname"
-        value={props.surnameValue}
-        onChange={props.surnameOnChangeHandler}
-        required
-      />
-      <input
+        className={styles.inputs}
         type="email"
         name="email"
         id="email"
@@ -44,6 +50,7 @@ const SignupForm = (props: SignupFormProps) => (
         required
       />
       <input
+        className={styles.inputs}
         type="password"
         name="password"
         id="password"
@@ -53,6 +60,7 @@ const SignupForm = (props: SignupFormProps) => (
         required
       />
       <input
+        className={styles.profilePic}
         type="file"
         name="profile-pic"
         id="profile-pic"
@@ -60,7 +68,58 @@ const SignupForm = (props: SignupFormProps) => (
         onChange={props.profilePicOnChangeHandler}
       />
     </div>
-    <button onClick={props.signUpOnClickHandler}>Sign up</button>
+    <p className={styles.tos}>
+      People who use our service may have uploaded your contact information to
+      Facebook.{" "}
+      <a
+        href="/help/637205020878504"
+        id="non-users-notice-link"
+        target="_blank"
+        rel="nofollow"
+      >
+        Learn more
+      </a>
+      .
+    </p>
+    <p className={styles.tos}>
+      By clicking Sign Up, you agree to our
+      <a
+        href="/legal/terms/update"
+        id="terms-link"
+        target="_blank"
+        rel="nofollow"
+      >
+        Terms
+      </a>
+      ,{" "}
+      <a
+        href="/about/privacy/update"
+        id="privacy-link"
+        target="_blank"
+        rel="nofollow"
+      >
+        Privacy Policy
+      </a>
+      and
+      <a
+        href="/policies/cookies/"
+        id="cookie-use-link"
+        target="_blank"
+        rel="nofollow"
+      >
+        Cookies Policy
+      </a>
+      . You may receive SMS notifications from us and can opt out at any time.
+    </p>
+    <button
+      className={styles.signupButton}
+      onClick={props.signUpOnClickHandler}
+    >
+      Sign Up
+    </button>
+    <a className={styles.redirectBtn} href="/login">
+      Already have an account?
+    </a>
   </form>
 );
 export default SignupForm;
