214590404945fcb6218671461a35ec3a1995e691
Hassane <hsisane5@gmail.com>
2023-02-24 18:09:00 +0100

    added styling to navbar component

--- src/components/homePage/navBar/NavBar.tsx
+++ src/components/homePage/navBar/NavBar.tsx
@@ -1,10 +1,11 @@
 import { SearchBar } from "./SearchBar";
 import { Navigation } from "./Navigation";
 import Settings from "./Settings";
+import styles from "../../../styles/homePage/navBar/navBar.module.css";
 
 export const NavBar = () => {
   return (
-    <div>
+    <div className={styles.navbar}>
       <SearchBar />
       <Navigation />
       <Settings />
