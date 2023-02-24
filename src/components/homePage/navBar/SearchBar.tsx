3c24075da8f5fac155f8db7a389bfdfdec602370
Hassane <hsisane5@gmail.com>
2023-02-24 18:07:44 +0100

    added styling to searchBar component

--- src/components/homePage/navBar/SearchBar.tsx
+++ src/components/homePage/navBar/SearchBar.tsx
@@ -1,11 +1,13 @@
 import { Logo, SearchIcon } from "../../../utils/svgsFunction";
+import styles from "../../../styles/homePage/navBar/searchBar.module.css";
+
 export const SearchBar = () => {
   return (
-    <div>
+    <div className={styles.searchBar}>
       <Logo />
       <label>
         <SearchIcon />
-        <input type="text" />
+        <input type="text" placeholder="Search Facebook" />
       </label>
     </div>
   );
