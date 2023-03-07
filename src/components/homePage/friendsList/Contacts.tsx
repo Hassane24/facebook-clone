e6f6ebf6ed5f1e3d0aab83a73de53cd5a7504dee
Hassane <hsisane5@gmail.com>
2023-03-07 19:36:14 +0100

    added Contacts component

---
+++ src/components/homePage/friendsList/Contacts.tsx
@@ -0,0 +1,16 @@
+import { Call, SearchIcon, Options } from "../../../utils/svgsFunction";
+
+export const Contacts = () => (
+  <div>
+    <span>Contacts</span>
+    <div>
+      <Call />
+    </div>
+    <div>
+      <SearchIcon />
+    </div>
+    <div>
+      <Options />
+    </div>
+  </div>
+);
