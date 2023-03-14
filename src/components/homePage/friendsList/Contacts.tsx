a098438e869126abcdeae30fe2d3a726865f068e
Hassane <hsisane5@gmail.com>
2023-03-14 19:43:21 +0100

    styling for contacts component

--- src/components/homePage/friendsList/Contacts.tsx
+++ src/components/homePage/friendsList/Contacts.tsx
@@ -1,16 +1,19 @@
 import { Call, SearchIcon, Options } from "../../../utils/svgsFunction";
+import styles from "../../../styles/homePage/friendsList/contacts.module.css";
 
 export const Contacts = () => (
-  <div>
+  <div className={styles.contacts}>
     <span>Contacts</span>
-    <div>
-      <Call />
-    </div>
-    <div>
-      <SearchIcon />
-    </div>
-    <div>
-      <Options />
+    <div className={styles.icons}>
+      <div>
+        <Call />
+      </div>
+      <div>
+        <SearchIcon />
+      </div>
+      <div>
+        <Options />
+      </div>
     </div>
   </div>
 );
