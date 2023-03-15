06ed96470781bac75d1af9232d7aa42ef67e9508
Hassane <hsisane5@gmail.com>
2023-03-15 20:44:08 +0100

    added an down arrow svg

--- src/utils/svgsFunction.tsx
+++ src/utils/svgsFunction.tsx
@@ -249,3 +249,11 @@ export const Options = () => (
     </g>
   </svg>
 );
+
+export const SeeMoreArrow = () => (
+  <svg fill="#e4e6eb" viewBox="0 0 16 16" width="1em" height="1em">
+    <g transform="translate(-448 -544)">
+      <path d="M452.707 549.293a1 1 0 0 0-1.414 1.414l4 4a1 1 0 0 0 1.414 0l4-4a1 1 0 0 0-1.414-1.414L456 552.586l-3.293-3.293z"></path>
+    </g>
+  </svg>
+);
