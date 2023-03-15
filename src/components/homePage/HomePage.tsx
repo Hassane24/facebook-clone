5a6ba21c8f75548f0965816158399ad2b62243f1
Hassane <hsisane5@gmail.com>
2023-03-15 21:50:50 +0100

    SideBar component

--- src/components/homePage/HomePage.tsx
+++ src/components/homePage/HomePage.tsx
@@ -1,12 +1,14 @@
 import { FriendsList } from "./friendsList/FriendsList";
 import { NavBar } from "./navBar/NavBar";
 import styles from "../../styles/homePage/homePage.module.css";
+import { SideBar } from "./sideBar/SideBar";
 const Home = () => {
   return (
     <div>
       <NavBar />
       <div className={styles.container}>
         <FriendsList />
+        <SideBar />
       </div>
     </div>
   );
---
+++ src/components/homePage/sideBar/SideBar.tsx
@@ -0,0 +1,86 @@
+import {
+  DefaultProfilePicture,
+  SeeMoreArrow,
+} from "../../../utils/svgsFunction";
+import Icons from "../../../assets/side-bar-icons.png";
+import { SideBarItem } from "./SideBarItem";
+import { useEffect, useState } from "react";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from "../../../firebase/firebase";
+import { storage } from "../../../firebase/firebase";
+import { ref, getDownloadURL } from "firebase/storage";
+
+export const SideBar = () => {
+  const [profileImageURL, setProfileImageURL] = useState<string>();
+  const [surname, setSurname] = useState<string>();
+  const [firstName, setFirstName] = useState<string>();
+  const sideBarItemsInfo = [
+    { name: "Find friends", imagePosition: "-304px" },
+    { name: "Groups", imagePosition: "-76px" },
+    { name: "Marketplace", imagePosition: "-418px" },
+    { name: "Watch", imagePosition: "-532px" },
+    { name: "Memories", imagePosition: "-456px" },
+    { name: "Saved", imagePosition: "-190px" },
+  ];
+
+  useEffect(() => {
+    const userID = localStorage.getItem("UserID");
+    getUserName(userID);
+    getUserImage(userID);
+  }, []);
+
+  const getUserImage = (userID: string | null) => {
+    getDownloadURL(ref(storage, `profile-pics/${userID}.png`))
+      .then((url) => {
+        setProfileImageURL(url);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const getUserName = (userID: string | null) => {
+    const querySnapshot = query(
+      collection(db, "users"),
+      where("userID", "==", userID)
+    );
+    getDocs(querySnapshot)
+      .then((docs) =>
+        docs.forEach((doc) => {
+          const userData = doc.data();
+          setSurname(userData.surname);
+          setFirstName(userData.firstName);
+        })
+      )
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div>
+      <div>
+        <div>
+          <DefaultProfilePicture userImage={profileImageURL} />
+        </div>
+        <span>
+          {surname} {firstName}
+        </span>
+      </div>
+      <SideBarItem sideBarItemName="Most recent" />
+      {sideBarItemsInfo.map((item, index) => (
+        <li key={index}>
+          <SideBarItem
+            sideBarItemName={item.name}
+            imagePosition={item.imagePosition}
+            icons={Icons}
+          />
+        </li>
+      ))}
+      <div>
+        <div>
+          <SeeMoreArrow />
+        </div>
+        <span>See more</span>
+      </div>
+    </div>
+  );
+};
