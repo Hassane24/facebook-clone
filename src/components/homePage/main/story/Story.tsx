b6315ee224ba8e50b806e1f4b6101912aca9b951
Hassane <hsisane5@gmail.com>
2023-03-17 18:34:40 +0100

    Added Story component

---
+++ src/components/homePage/main/story/Story.tsx
@@ -0,0 +1,10 @@
+import { CreateStroyCard } from "./CreateStoryCard";
+import { StoryReel } from "./StoryReel";
+export const Story = () => {
+  return (
+    <div>
+      <StoryReel />
+      <CreateStroyCard />
+    </div>
+  );
+};
