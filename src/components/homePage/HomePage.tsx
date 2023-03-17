a8ba4a371dcba51c445cfc3c6dc9d83248500ce2
Hassane <hsisane5@gmail.com>
2023-03-17 18:43:28 +0100

    Added Main to HomePage component

--- src/components/homePage/HomePage.tsx
+++ src/components/homePage/HomePage.tsx
@@ -2,13 +2,15 @@ import { FriendsList } from "./friendsList/FriendsList";
 import { NavBar } from "./navBar/NavBar";
 import styles from "../../styles/homePage/homePage.module.css";
 import { SideBar } from "./sideBar/SideBar";
+import { Main } from "./main/Main";
 const Home = () => {
   return (
     <div>
       <NavBar />
       <div className={styles.container}>
-        <FriendsList />
         <SideBar />
+        <Main />
+        <FriendsList />
       </div>
     </div>
   );
