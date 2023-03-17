989a262b41feea40ffa16276e57c9c62a9a67251
Hassane <hsisane5@gmail.com>
2023-03-17 18:33:54 +0100

    Added CreateStoryCard component

---
+++ src/components/homePage/main/story/CreateStoryCard.tsx
@@ -0,0 +1,19 @@
+import { AddStoryIcon } from "../../../../utils/svgsFunction";
+
+export const CreateStroyCard = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <AddStoryIcon />
+        </div>
+        <div>
+          <span>Create story</span>
+        </div>
+      </div>
+    </div>
+  );
+};
