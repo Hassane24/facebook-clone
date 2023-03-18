8c6a2bd9dd3175d0d1f02b348c449433518185d2
Hassane <hsisane5@gmail.com>
2023-03-18 20:50:06 +0100

    CreateStoryCard styling almost done

--- src/components/homePage/main/story/CreateStoryCard.tsx
+++ src/components/homePage/main/story/CreateStoryCard.tsx
@@ -1,13 +1,14 @@
 import { AddStoryIcon } from "../../../../utils/svgsFunction";
-
+import styles from "../../../../styles/homePage/main/createStoryCard.module.css";
+import profilePic from "../../../../assets/Default_pfp.png";
 export const CreateStroyCard = () => {
   return (
-    <div>
-      <div>
-        <div>
-          <img src="" alt="" />
+    <div style={{ display: "flex", margin: "16px auto 16px 16px" }}>
+      <div className={styles.createStoryCard}>
+        <div className={styles.imageHolder}>
+          <img src={profilePic} alt="" />
         </div>
-        <div>
+        <div className={styles.addStoryIcon}>
           <AddStoryIcon />
         </div>
         <div>
