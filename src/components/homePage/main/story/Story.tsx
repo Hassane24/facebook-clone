23e863f2fa04b742eaa315a05954b885cd36be40
Hassane <hsisane5@gmail.com>
2023-03-19 21:18:02 +0000

    Code refactoring

--- src/components/homePage/main/story/Story.tsx
+++ src/components/homePage/main/story/Story.tsx
@@ -1,27 +1,16 @@
 import { CreateStroyCard } from "./CreateStoryCard";
-import { ref, getDownloadURL } from "firebase/storage";
-import { storage } from "../../../../firebase/firebase";
-import { useEffect, useState } from "react";
-
 import { StoryReel } from "./StoryReel";
+import { fetchUserImage } from "../../../../utils/fetchUserImage";
+import { useEffect, useState } from "react";
 import styles from "../../../../styles/homePage/main/story.module.css";
 export const Story = () => {
   const [profileImageURL, setProfileImageURL] = useState<string>("");
 
   useEffect(() => {
     const userID = localStorage.getItem("UserID");
-    getUserImage(userID);
+    const getUserImage = async () => await fetchUserImage(userID);
+    getUserImage().then((imageLink) => setProfileImageURL(imageLink));
   }, []);
-
-  const getUserImage = (userID: string | null) => {
-    getDownloadURL(ref(storage, `profile-pics/${userID}.png`))
-      .then((url) => {
-        setProfileImageURL(url);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
 
   return (
     <div className={styles.story}>
