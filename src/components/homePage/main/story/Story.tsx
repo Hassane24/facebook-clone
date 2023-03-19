d8b5bbf66abf2263768dca023bfa1c1c36c99499
Hassane <hsisane5@gmail.com>
2023-03-19 19:35:10 +0000

    Story component now fetches the user image and displays it on createStoryCard

--- src/components/homePage/main/story/Story.tsx
+++ src/components/homePage/main/story/Story.tsx
@@ -1,11 +1,32 @@
 import { CreateStroyCard } from "./CreateStoryCard";
+import { ref, getDownloadURL } from "firebase/storage";
+import { storage } from "../../../../firebase/firebase";
+import { useEffect, useState } from "react";
+
 import { StoryReel } from "./StoryReel";
 import styles from "../../../../styles/homePage/main/story.module.css";
 export const Story = () => {
+  const [profileImageURL, setProfileImageURL] = useState<string>("");
+
+  useEffect(() => {
+    const userID = localStorage.getItem("UserID");
+    getUserImage(userID);
+  }, []);
+
+  const getUserImage = (userID: string | null) => {
+    getDownloadURL(ref(storage, `profile-pics/${userID}.png`))
+      .then((url) => {
+        setProfileImageURL(url);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className={styles.story}>
       <StoryReel />
-      <CreateStroyCard />
+      <CreateStroyCard userImageUrl={profileImageURL} />
     </div>
   );
 };
