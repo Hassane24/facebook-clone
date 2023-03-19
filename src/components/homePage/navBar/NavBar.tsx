9a2cca35d0be1ec5fea424b087fe86450ff6c139
Hassane <hsisane5@gmail.com>
2023-03-19 21:17:38 +0000

    Code refactoring

--- src/components/homePage/navBar/NavBar.tsx
+++ src/components/homePage/navBar/NavBar.tsx
@@ -1,55 +1,33 @@
-import { ref, getDownloadURL } from "firebase/storage";
-import { collection, query, where, getDocs } from "firebase/firestore";
-import { db } from "../../../firebase/firebase";
-import { storage } from "../../../firebase/firebase";
 import { SearchBar } from "./SearchBar";
 import { Navigation } from "./Navigation";
 import Settings from "./Settings";
 import styles from "../../../styles/homePage/navBar/navBar.module.css";
 import { useEffect, useState } from "react";
+import { fetchUserImage } from "../../../utils/fetchUserImage";
+import { fetchUserName } from "../../../utils/fetchUserName";
 
 export const NavBar = () => {
-  const [profileImageURL, setProfileImageURL] = useState<string>();
+  const [profileImageURL, setProfileImageURL] = useState<string>("");
   const [surname, setSurname] = useState<string>();
   const [firstName, setFirstName] = useState<string>();
   useEffect(() => {
     const userID = localStorage.getItem("UserID");
-    getUserImage(userID);
-    getUserName(userID);
+    const getUserName = async () => await fetchUserName(userID);
+    getUserName().then((userName) => {
+      setFirstName(userName[0].firstName);
+      setSurname(userName[0].surname);
+    });
+    const getUserImage = async () => await fetchUserImage(userID);
+    getUserImage().then((imageLink) => setProfileImageURL(imageLink));
   }, []);
 
-  const getUserImage = (userID: string | null) => {
-    getDownloadURL(ref(storage, `profile-pics/${userID}.png`))
-      .then((url) => {
-        setProfileImageURL(url);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
-  const getUserName = (userID: string | null) => {
-    const querySnapshot = query(
-      collection(db, "users"),
-      where("userID", "==", userID)
-    );
-    getDocs(querySnapshot)
-      .then((docs) =>
-        docs.forEach((doc) => {
-          const userData = doc.data();
-          setSurname(userData.surname);
-          setFirstName(userData.firstName);
-        })
-      )
-      .catch((err) => console.log(err));
-  };
   return (
     <div className={styles.navbar}>
       <SearchBar />
       <Navigation />
       <Settings
-        profilePicture={profileImageURL as string}
-        userImage={profileImageURL!}
+        profilePicture={profileImageURL}
+        userImage={profileImageURL}
         surname={surname}
         firstName={firstName}
       />
