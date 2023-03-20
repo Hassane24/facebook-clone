df746564e151ad492711e5890f0d203c351fdefa
Hassane <hsisane5@gmail.com>
2023-03-20 18:48:30 +0100

    Added WhatsOnYourMind to Main component

--- src/components/homePage/main/Main.tsx
+++ src/components/homePage/main/Main.tsx
@@ -1,10 +1,12 @@
 import { Story } from "./story/Story";
 import styles from "../../../styles/homePage/main/main.module.css";
+import { WhatsOnYourMind } from "./whatsOnYourMind/WhatsOnYourMind";
 
 export const Main = () => {
   return (
     <div className={styles.main}>
       <Story />
+      <WhatsOnYourMind />
     </div>
   );
 };
