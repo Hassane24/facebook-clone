9358ef7574a73dd61050e00ff61e96248cbc0cbf
Hassane <hsisane5@gmail.com>
2023-03-20 18:40:02 +0100

    Styling of WhatsOnYourMind component

--- src/components/homePage/main/whatsOnYourMind/WhatsOnYourMind.tsx
+++ src/components/homePage/main/whatsOnYourMind/WhatsOnYourMind.tsx
@@ -2,34 +2,35 @@ import Live from "../../../../assets/live-video.png";
 import Photos from "../../../../assets/images.png";
 import Feeling from "../../../../assets/feeling.png";
 import { DefaultProfilePicture } from "../../../../utils/svgsFunction";
-
+import styles from "../../../../styles/homePage/main/whatsOnYourMind/whatsOnYourMind.module.css";
 export const WhatsOnYourMind = () => {
   return (
-    <div>
-      <div>
-        <div>
+    <div className={styles.container}>
+      <div className={styles.whatsOnYourMind}>
+        <div className={styles.imageHolder}>
           <DefaultProfilePicture />
+          <div className={styles.overlayDiv}></div>
         </div>
-        <div>
+        <div className={styles.onYourMindText}>
           <span>What's on your mind, Hassane?</span>
         </div>
       </div>
-      <div>
+      <div className={styles.choices}>
         <div>
           <div>
-            <img src={Live} alt="" />
+            <img src={Live} alt="" width="24px" height="24px" />
           </div>
           <span>Live video</span>
         </div>
         <div>
           <div>
-            <img src={Photos} alt="" />
+            <img src={Photos} alt="" width="24px" height="24px" />
           </div>
           <span>Photo/video</span>
         </div>
         <div>
           <div>
-            <img src={Feeling} alt="" />
+            <img src={Feeling} alt="" width="24px" height="24px" />
           </div>
           <span>Feeling/activity</span>
         </div>
