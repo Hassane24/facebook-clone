56e697ff733fe5f5a93bdf65cab12a0650209a5c
Hassane <hsisane5@gmail.com>
2023-03-20 18:47:44 +0100

    WhatOnYourMind now fetches the user's first name and pfp

--- src/components/homePage/main/whatsOnYourMind/WhatsOnYourMind.tsx
+++ src/components/homePage/main/whatsOnYourMind/WhatsOnYourMind.tsx
@@ -3,16 +3,29 @@ import Photos from "../../../../assets/images.png";
 import Feeling from "../../../../assets/feeling.png";
 import { DefaultProfilePicture } from "../../../../utils/svgsFunction";
 import styles from "../../../../styles/homePage/main/whatsOnYourMind/whatsOnYourMind.module.css";
+import { fetchUserImage } from "../../../../utils/fetchUserImage";
+import { fetchUserName } from "../../../../utils/fetchUserName";
+import { useEffect, useState } from "react";
 export const WhatsOnYourMind = () => {
+  const [profileImageURL, setProfileImageURL] = useState<string>("");
+  const [firstName, setFirstName] = useState<string>();
+  useEffect(() => {
+    const userID = localStorage.getItem("UserID");
+    const getUserName = async () => await fetchUserName(userID);
+    getUserName().then((userName) => setFirstName(userName[0].firstName));
+    const getUserImage = async () => await fetchUserImage(userID);
+    getUserImage().then((imageLink) => setProfileImageURL(imageLink));
+  }, []);
+
   return (
     <div className={styles.container}>
       <div className={styles.whatsOnYourMind}>
         <div className={styles.imageHolder}>
-          <DefaultProfilePicture />
+          <DefaultProfilePicture userImage={profileImageURL} />
           <div className={styles.overlayDiv}></div>
         </div>
         <div className={styles.onYourMindText}>
-          <span>What's on your mind, Hassane?</span>
+          <span>What's on your mind, {firstName}?</span>
         </div>
       </div>
       <div className={styles.choices}>
