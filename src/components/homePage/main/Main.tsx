7cef7d4b494630591edda47abae80b9e668e3533
Hassane <hsisane5@gmail.com>
2023-03-27 00:17:07 +0200

    Added PostCard.tsx to Main.tsx

--- src/components/homePage/main/Main.tsx
+++ src/components/homePage/main/Main.tsx
@@ -1,12 +1,14 @@
 import { Story } from "./story/Story";
 import styles from "../../../styles/homePage/main/main.module.css";
 import { WhatsOnYourMind } from "./whatsOnYourMind/WhatsOnYourMind";
+import { PostCard } from "./postCard/PostCard";
 
 export const Main = () => {
   return (
     <div className={styles.main}>
       <Story />
       <WhatsOnYourMind />
+      <PostCard />
     </div>
   );
 };
