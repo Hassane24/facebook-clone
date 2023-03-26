be0cf34a7f32e7fcef29156371533bc0d0727df3
Hassane <hsisane5@gmail.com>
2023-03-27 01:28:28 +0200

    HTML template for PostContent.tsx

---
+++ src/components/homePage/main/postCard/PostContent.tsx
@@ -0,0 +1,14 @@
+import { Angry } from "../../../../utils/svgsFunction";
+
+export const PostContent = () => {
+  return (
+    <div>
+      <div>
+        <Angry />
+      </div>
+      <div>
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+};
