3a1b558d5e1f27329d986500ce5462103dd44b31
Hassane <hsisane5@gmail.com>
2023-03-28 03:16:07 +0100

    PostContent.tsx styling

--- src/components/homePage/main/postCard/PostContent.tsx
+++ src/components/homePage/main/postCard/PostContent.tsx
@@ -1,8 +1,9 @@
+import styles from "../../../../styles/homePage/main/postCard/postContent.module.css";
 export const PostContent = () => {
   return (
-    <div>
-      <div></div>
-      <div>
+    <div className={styles.contentContainer}>
+      <div className={styles.textContent}></div>
+      <div className={styles.imageHolder}>
         <img src="" alt="" />
       </div>
     </div>
