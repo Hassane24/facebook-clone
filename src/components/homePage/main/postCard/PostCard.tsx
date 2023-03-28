ba12baa2f0afed0c2c03549abde3f0326aaeacb0
Hassane <hsisane5@gmail.com>
2023-03-28 02:25:36 +0100

    styling for PostInfo.tsx

--- src/components/homePage/main/postCard/PostCard.tsx
+++ src/components/homePage/main/postCard/PostCard.tsx
@@ -1,10 +1,10 @@
 import { InteractWithPost } from "./InteractWithPost";
 import { PostContent } from "./PostContent";
 import { PostInfo } from "./PostInfo";
-
+import styles from "../../../../styles/homePage/main/postCard/postCard.module.css";
 export const PostCard = () => {
   return (
-    <div>
+    <div className={styles.postCardContainer}>
       <PostInfo />
       <PostContent />
       <InteractWithPost />
--- src/components/homePage/main/postCard/PostInfo.tsx
+++ src/components/homePage/main/postCard/PostInfo.tsx
@@ -1,38 +1,49 @@
 import { DefaultProfilePicture, Options } from "../../../../utils/svgsFunction";
 import Public from "../../../../assets/public.png";
 import utilityIcons from "../../../../assets/utility-icons.png";
+import styles from "../../../../styles/homePage/main/postCard/postInfo.module.css";
 export const PostInfo = () => {
   return (
-    <div>
+    <div className={styles.postInfoContainer}>
       <div>
-        <DefaultProfilePicture />
-      </div>
-      <div>
-        <span>firstName surname</span>
-        <div>
-          <span>date</span>
-          <span>.</span>
-          <span>
-            <img src={Public} alt="" />
-          </span>
+        <div className={styles.imageHolder}>
+          <DefaultProfilePicture />
+          <div className={styles.imageOverlay}></div>
         </div>
         <div>
-          <div>
-            <i
-              style={{
-                backgroundImage: `url(${utilityIcons})`,
-                backgroundPosition: "-66px -110px",
-                backgroundSize: "190px 204px",
-                width: "20px",
-                height: "20px",
-                backgroundRepeat: "no-repeat",
-                display: "inline-block",
-              }}
-            ></i>
+          <span className={styles.username}>firstName surname</span>
+          <div className={styles.date}>
+            <div>date</div>
+            <div>.</div>
+            <div>
+              <img
+                src={Public}
+                alt=""
+                width={"12px"}
+                height="12px"
+                className={styles.icons}
+              />
+            </div>
           </div>
-          <div>
-            <Options />
-          </div>
+        </div>
+      </div>
+      <div>
+        <div className={styles.options}>
+          <Options />
+        </div>
+        <div className={styles.options}>
+          <i
+            className={styles.icons}
+            style={{
+              backgroundImage: `url(${utilityIcons})`,
+              backgroundPosition: "-66px -110px",
+              backgroundSize: "190px 204px",
+              width: "20px",
+              height: "20px",
+              backgroundRepeat: "no-repeat",
+              display: "inline-block",
+            }}
+          ></i>
         </div>
       </div>
     </div>
