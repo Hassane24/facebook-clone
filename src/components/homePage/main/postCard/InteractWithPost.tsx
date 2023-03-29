3f2006c9e1303e7b8b31ce9ddaa41d455b2b1f28
Hassane <hsisane5@gmail.com>
2023-03-29 03:51:13 +0100

    InteractWithPost.tsx basic styling

--- src/components/homePage/main/postCard/InteractWithPost.tsx
+++ src/components/homePage/main/postCard/InteractWithPost.tsx
@@ -1,37 +1,82 @@
+import styles from "../../../../styles/homePage/main/postCard/interactWithPost.module.css";
+import haha from "../../../../assets/haha.png";
+import love from "../../../../assets/love.png";
+import like from "../../../../assets/like.png";
 import utilityIcons from "../../../../assets/utility-icons-3.png";
 export const InteractWithPost = () => {
   return (
-    <div>
-      <div>
-        <div></div>
-        <div></div>
-      </div>
-      <div>
-        <div>
-          <i
-            style={{
-              backgroundImage: `url(${utilityIcons})`,
-              backgroundPosition: "0px -504px",
-              backgroundSize: "26px 858px",
-              width: "16px",
-              height: "16px",
-              backgroundRepeat: "no-repeat",
-              display: "inline-block",
-            }}
-          ></i>
+    <div className={styles.interactionsContainer}>
+      <div className={styles.firstContainer}>
+        <div className={styles.interactions}>
+          <div>
+            <div>
+              <img src={like} alt="" height={"18px"} width="18px" />
+            </div>
+            <div>
+              <img src={love} alt="" height={"18px"} width="18px" />
+            </div>
+            <div>
+              <img src={haha} alt="" height={"18px"} width="18px" />
+            </div>
+          </div>
+          <div className={styles.numberOfInteractions}>1.3k</div>
         </div>
-        <div>
-          <i
-            style={{
-              backgroundImage: `url(${utilityIcons})`,
-              backgroundPosition: "0px -684px",
-              backgroundSize: "26px 858px",
-              width: "16px",
-              height: "16px",
-              backgroundRepeat: "no-repeat",
-              display: "inline-block",
-            }}
-          ></i>
+        <div className={styles.commentsAndShares}>
+          <div>12 comments</div>
+          <div>12 shares</div>
+        </div>
+      </div>
+      <div className={styles.secondContainer}>
+        <div className={styles.interaction}>
+          <div>
+            <i
+              className={styles.icons}
+              style={{
+                backgroundImage: `url(${utilityIcons})`,
+                backgroundPosition: "0px -339px",
+                backgroundSize: "26px 866px",
+                width: "18px",
+                height: "18px",
+                backgroundRepeat: "no-repeat",
+                display: "inline-block",
+              }}
+            ></i>
+          </div>
+          Like
+        </div>
+        <div className={styles.interaction}>
+          <div>
+            <i
+              className={styles.icons}
+              style={{
+                backgroundImage: `url(${utilityIcons})`,
+                backgroundPosition: "0px -299px",
+                backgroundSize: "26px 866px",
+                width: "18px",
+                height: "18px",
+                backgroundRepeat: "no-repeat",
+                display: "inline-block",
+              }}
+            ></i>
+          </div>
+          Comment
+        </div>
+        <div className={styles.interaction}>
+          <div>
+            <i
+              className={styles.icons}
+              style={{
+                backgroundImage: `url(${utilityIcons})`,
+                backgroundPosition: "0px -688px",
+                backgroundSize: "26px 866px",
+                width: "18px",
+                height: "18px",
+                backgroundRepeat: "no-repeat",
+                display: "inline-block",
+              }}
+            ></i>
+          </div>
+          Share
         </div>
       </div>
     </div>
