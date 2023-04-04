9f8aaca8d6b1b13329528e97c116977a9347d47a
Hassane <hsisane5@gmail.com>
2023-04-04 02:25:45 +0100

    added props and conditional rendering on the image

--- src/components/homePage/main/postCard/PostContent.tsx
+++ src/components/homePage/main/postCard/PostContent.tsx
@@ -1,11 +1,19 @@
 import styles from "../../../../styles/homePage/main/postCard/postContent.module.css";
-export const PostContent = () => {
+export const PostContent = ({
+  postText,
+  postImage,
+}: {
+  postText: string;
+  postImage: string | null;
+}) => {
   return (
     <div className={styles.contentContainer}>
-      <div className={styles.textContent}></div>
-      <div className={styles.imageHolder}>
-        <img src="" alt="" />
-      </div>
+      <div className={styles.textContent}>{postText}</div>
+      {postImage && (
+        <div className={styles.imageHolder}>
+          <img src={postImage} alt="" />
+        </div>
+      )}
     </div>
   );
 };
