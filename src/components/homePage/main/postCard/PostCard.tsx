b772c2b8a009131b29eca059ed6fcb814502f9d5
Hassane <hsisane5@gmail.com>
2023-04-04 02:26:20 +0100

    added props

--- src/components/homePage/main/postCard/PostCard.tsx
+++ src/components/homePage/main/postCard/PostCard.tsx
@@ -2,11 +2,24 @@ import { InteractWithPost } from "./InteractWithPost";
 import { PostContent } from "./PostContent";
 import { PostInfo } from "./PostInfo";
 import styles from "../../../../styles/homePage/main/postCard/postCard.module.css";
-export const PostCard = () => {
+
+interface postCardProps {
+  firstName: string;
+  surname: string;
+  pfpURL: string;
+  postText: string;
+  postImage: string;
+}
+
+export const PostCard = (props: postCardProps) => {
   return (
     <div className={styles.postCardContainer}>
-      <PostInfo />
-      <PostContent />
+      <PostInfo
+        pfpURL={props.pfpURL}
+        firstName={props.firstName}
+        surname={props.surname}
+      />
+      <PostContent postText={props.postText} postImage={props.postImage} />
       <InteractWithPost />
     </div>
   );
