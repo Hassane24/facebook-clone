e442cdc3c83585be25d27eb874f7b094c8d1251c
Hassane <hsisane5@gmail.com>
2023-04-04 04:22:24 +0100

    lifted the state to Main.tsx and forwardRefed textArea node to Main.tsx

--- src/components/homePage/main/whatsOnYourMind/CreatePost.tsx
+++ src/components/homePage/main/whatsOnYourMind/CreatePost.tsx
@@ -14,11 +14,7 @@ import {
   SmileyFace,
   Options,
 } from "../../../../utils/svgsFunction";
-import { useState } from "react";
-import { storage, db } from "../../../../firebase/firebase";
-import { doc, setDoc } from "firebase/firestore";
-
-import { uploadBytes, getDownloadURL, ref } from "firebase/storage";
+import { useState, forwardRef } from "react";
 
 interface CreatePostProps {
   popUpPictureAddingArea: boolean;
@@ -26,235 +22,52 @@ interface CreatePostProps {
   firstName: string;
   surname: string;
   userProfilePicture: string;
+  image: File | null;
   closeCreatePost: () => void;
   closePictureAddingArea: () => void;
   openPictureAddingArea: () => void;
   setWhatsOnYourMindValue: (value: string) => void;
+  inputFileHandler: (e?: React.ChangeEvent<HTMLInputElement>) => Promise<void>;
 }
 
-export const CreatePost = (props: CreatePostProps) => {
-  const [textAreaValue, setTextAreaValue] = useState("");
-  const [image, setImage] = useState<File | null>(null);
-  const [imageUrl, setImageUrl] = useState<
-    string | null | undefined | ArrayBuffer
-  >("");
+export const CreatePost = forwardRef<HTMLTextAreaElement, CreatePostProps>(
+  (props, ref) => {
+    const [textAreaValue, setTextAreaValue] = useState("");
+    const [imageUrl, setImageUrl] = useState<
+      string | null | undefined | ArrayBuffer
+    >("");
 
-  const textAreaHandler = (e: React.ChangeEvent<HTMLTextAreaElement>) =>
-    setTextAreaValue(e.target.value);
+    const textAreaHandler = (e: React.ChangeEvent<HTMLTextAreaElement>) =>
+      setTextAreaValue(e.target.value);
 
-  const inputFileHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) {
-      let reader = new FileReader();
-      reader.onload = (event) => setImageUrl(event.target?.result);
-      reader.readAsDataURL(e.target.files[0]);
-      setImage(e.target.files[0]);
-    }
-  };
-
-  const postStatus = async () => {
-    try {
-      const firstName = localStorage.getItem("first-name");
-      const surname = localStorage.getItem("surname");
-      const userPfpUrl = localStorage.getItem("profile-picture");
-
-      if (image) {
-        const imageRef = ref(storage, `post-images/${image?.lastModified}.png`);
-        await uploadBytes(imageRef, image);
-        const postPictureURL = await getDownloadURL(imageRef);
-        await setDoc(doc(db, "posts", `${new Date().getTime()}`), {
-          postName: new Date().getTime(),
-          postText: textAreaValue,
-          pictureUrl: postPictureURL,
-          userPfpUrl: userPfpUrl,
-          firstName: firstName,
-          surname: surname,
-          reactions: {
-            like: 0,
-            love: 0,
-            haha: 0,
-            sad: 0,
-            wow: 0,
-            care: 0,
-            angry: 0,
-          },
-        });
-      } else {
-        await setDoc(doc(db, "posts", `${new Date().getTime()}`), {
-          postName: new Date().getTime(),
-
-          postText: textAreaValue,
-          pictureUrl: null,
-          userPfpUrl: userPfpUrl,
-          firstName: firstName,
-          surname: surname,
-          reactions: {
-            like: 0,
-            love: 0,
-            haha: 0,
-            sad: 0,
-            wow: 0,
-            care: 0,
-            angry: 0,
-          },
-        });
+    const displayImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (e.target.files) {
+        let reader = new FileReader();
+        reader.onload = (event) => setImageUrl(event.target?.result);
+        reader.readAsDataURL(e.target.files[0]);
       }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+    };
 
-  return (
-    <div
-      style={{ padding: props.popUpPictureAddingArea ? "" : "100px" }}
-      className={`${styles.formContainer} ${
-        props.popUp ? styles.active : undefined
-      }`}
-    >
-      <form
-        className={`${styles.form} ${props.popUp ? styles.active : undefined}`}
+    return (
+      <div
+        style={{ padding: props.popUpPictureAddingArea ? "" : "100px" }}
+        className={`${styles.formContainer} ${
+          props.popUp ? styles.active : undefined
+        }`}
       >
-        <div className={styles.firstContainer}>
-          <div className={styles.createPost}>
-            <div className={styles.createPostText}>Create post</div>
-            <div
-              className={styles.closeFormButton}
-              onClick={() => {
-                props.setWhatsOnYourMindValue(textAreaValue);
-                props.closeCreatePost();
-              }}
-            >
-              <i
-                className={styles.icon}
-                style={{
-                  backgroundImage: `url(${utilityIcons})`,
-                  backgroundPosition: "-66px -110px",
-                  backgroundSize: "190px 204px",
-                  width: "20px",
-                  height: "20px",
-                  backgroundRepeat: "no-repeat",
-                  display: "inline-block",
-                }}
-              ></i>
-            </div>
-          </div>
-
-          <div className={styles.privacy}>
-            <div>
-              <DefaultProfilePicture userImage={props.userProfilePicture} />
-              <div></div>
-            </div>
-            <div>
-              <span>
-                {props.firstName} {props.surname}
-              </span>
-              <div className={styles.options}>
-                <div>
-                  <img
-                    src={Public}
-                    alt=""
-                    height="12px"
-                    width="12px"
-                    className={styles.icon}
-                  />
-                </div>
-                <span>Public</span>
-                <i
-                  className={styles.icon}
-                  style={{
-                    backgroundImage: `url(${utilityIcons})`,
-                    backgroundPosition: "-166px -172px",
-                    backgroundSize: "190px 204px",
-                    width: "12px",
-                    height: "12px",
-                    display: "inline-block",
-                    backgroundRepeat: "no-repeat",
-                  }}
-                ></i>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className={styles.secondContainer}>
-          <div
-            className={`${
-              props.popUpPictureAddingArea ? styles.textArea : undefined
-            } ${
-              !props.popUpPictureAddingArea ? styles.biggerTextArea : undefined
-            }`}
-          >
-            {!props.popUpPictureAddingArea && textAreaValue.length < 140 && (
-              <div>
-                <img src={Aa} alt="" height={"38px"} width="38px" />
-              </div>
-            )}
-            <textarea
-              wrap="soft"
-              placeholder={`What's on your mind, ${props.firstName}?`}
-              value={textAreaValue}
-              onChange={textAreaHandler}
-            />
-            <div
-              className={
-                !props.popUpPictureAddingArea && textAreaValue.length >= 140
-                  ? styles.smileyFace
-                  : undefined
-              }
-            >
-              <SmileyFace />
-            </div>
-          </div>
-
-          {props.popUpPictureAddingArea && (
-            <div className={`${styles.pictureAddingArea}`}>
-              <div>
-                {!imageUrl && (
-                  <>
-                    <input
-                      type="file"
-                      name="image"
-                      id="image"
-                      title=""
-                      accept="image/png, image/jpeg"
-                      onChange={inputFileHandler}
-                    />
-                    <div>
-                      <i
-                        className={styles.icon}
-                        style={{
-                          backgroundImage: `url(${secondUtilityIcons})`,
-                          backgroundPosition: "0px -86px",
-                          backgroundSize: "38px 162px",
-                          width: "20px",
-                          height: "20px",
-                          backgroundRepeat: "no-repeat",
-                          display: "inline-block",
-                        }}
-                      ></i>
-                    </div>
-                    <div>Add photos/videos</div>
-                    <div>or drag and drop</div>
-                  </>
-                )}
-                {imageUrl && (
-                  <div className={styles.imageHolder}>
-                    <img src={imageUrl as string} alt="" />
-                  </div>
-                )}
-              </div>
-              <div className={styles.photosFromHome}>
-                <div>
-                  <Phone />
-                </div>
-                <div>Add photos and videos from your mobile device.</div>
-                <div>Add</div>
-              </div>
+        <form
+          className={`${styles.form} ${
+            props.popUp ? styles.active : undefined
+          }`}
+        >
+          <div className={styles.firstContainer}>
+            <div className={styles.createPost}>
+              <div className={styles.createPostText}>Create post</div>
               <div
-                className={styles.closeDragDrop}
+                className={styles.closeFormButton}
                 onClick={() => {
-                  props.closePictureAddingArea();
-                  setImage(null);
-                  setImageUrl(null);
+                  props.setWhatsOnYourMindValue(textAreaValue);
+                  props.closeCreatePost();
                 }}
               >
                 <i
@@ -271,67 +84,218 @@ export const CreatePost = (props: CreatePostProps) => {
                 ></i>
               </div>
             </div>
-          )}
-        </div>
 
-        <div className={styles.thirdContainer}>
-          <div>
-            <div>Add to your post</div>
-            <div>
-              <div
-                style={{
-                  backgroundColor: props.popUpPictureAddingArea ? "black" : "",
-                }}
-                onClick={props.openPictureAddingArea}
-              >
-                <img src={images} alt="" height="24px" width="24px" />
+            <div className={styles.privacy}>
+              <div>
+                <DefaultProfilePicture userImage={props.userProfilePicture} />
+                <div></div>
               </div>
               <div>
-                <img src={tagPeople} alt="" height="24px" width="24px" />
-              </div>
-              <div>
-                <img src={feeling} alt="" height="24px" width="24px" />
-              </div>
-              <div>
-                <img src={checkIn} alt="" height="24px" width="24px" />
-              </div>
-              <div>
-                <img src={lifeEvent} alt="" height="24px" width="24px" />
-              </div>
-              <div>
-                <Options />
+                <span>
+                  {props.firstName} {props.surname}
+                </span>
+                <div className={styles.options}>
+                  <div>
+                    <img
+                      src={Public}
+                      alt=""
+                      height="12px"
+                      width="12px"
+                      className={styles.icon}
+                    />
+                  </div>
+                  <span>Public</span>
+                  <i
+                    className={styles.icon}
+                    style={{
+                      backgroundImage: `url(${utilityIcons})`,
+                      backgroundPosition: "-166px -172px",
+                      backgroundSize: "190px 204px",
+                      width: "12px",
+                      height: "12px",
+                      display: "inline-block",
+                      backgroundRepeat: "no-repeat",
+                    }}
+                  ></i>
+                </div>
               </div>
             </div>
           </div>
-          <div
-            className={
-              image || textAreaValue !== "" ? styles.blueDiv : undefined
-            }
-          >
-            <button
-              className={
-                image || textAreaValue !== "" ? styles.blueButton : undefined
-              }
-              type="button"
+
+          <div className={styles.secondContainer}>
+            <div
+              className={`${
+                props.popUpPictureAddingArea ? styles.textArea : undefined
+              } ${
+                !props.popUpPictureAddingArea
+                  ? styles.biggerTextArea
+                  : undefined
+              }`}
             >
-              Post
-            </button>
-            {/* REMINDER: the post picture method goes on this div */}
-            {image || textAreaValue !== "" ? (
-              <div onClick={postStatus}></div>
-            ) : null}
+              {!props.popUpPictureAddingArea && textAreaValue.length < 140 && (
+                <div>
+                  <img src={Aa} alt="" height={"38px"} width="38px" />
+                </div>
+              )}
+              <textarea
+                wrap="soft"
+                placeholder={`What's on your mind, ${props.firstName}?`}
+                value={textAreaValue}
+                onChange={textAreaHandler}
+                ref={ref}
+              />
+              <div
+                className={
+                  !props.popUpPictureAddingArea && textAreaValue.length >= 140
+                    ? styles.smileyFace
+                    : undefined
+                }
+              >
+                <SmileyFace />
+              </div>
+            </div>
+
+            {props.popUpPictureAddingArea && (
+              <div className={`${styles.pictureAddingArea}`}>
+                <div>
+                  {!imageUrl && (
+                    <>
+                      <input
+                        type="file"
+                        name="image"
+                        id="image"
+                        title=""
+                        accept="image/png, image/jpeg"
+                        onChange={(e) => {
+                          displayImage(e);
+                          props.inputFileHandler(e);
+                        }}
+                      />
+                      <div>
+                        <i
+                          className={styles.icon}
+                          style={{
+                            backgroundImage: `url(${secondUtilityIcons})`,
+                            backgroundPosition: "0px -86px",
+                            backgroundSize: "38px 162px",
+                            width: "20px",
+                            height: "20px",
+                            backgroundRepeat: "no-repeat",
+                            display: "inline-block",
+                          }}
+                        ></i>
+                      </div>
+                      <div>Add photos/videos</div>
+                      <div>or drag and drop</div>
+                    </>
+                  )}
+                  {imageUrl && (
+                    <div className={styles.imageHolder}>
+                      <img src={imageUrl as string} alt="" />
+                    </div>
+                  )}
+                </div>
+                <div className={styles.photosFromHome}>
+                  <div>
+                    <Phone />
+                  </div>
+                  <div>Add photos and videos from your mobile device.</div>
+                  <div>Add</div>
+                </div>
+                <div
+                  className={styles.closeDragDrop}
+                  onClick={() => {
+                    props.closePictureAddingArea();
+                    setImageUrl(null);
+                  }}
+                >
+                  <i
+                    className={styles.icon}
+                    style={{
+                      backgroundImage: `url(${utilityIcons})`,
+                      backgroundPosition: "-66px -110px",
+                      backgroundSize: "190px 204px",
+                      width: "20px",
+                      height: "20px",
+                      backgroundRepeat: "no-repeat",
+                      display: "inline-block",
+                    }}
+                  ></i>
+                </div>
+              </div>
+            )}
           </div>
-        </div>
-      </form>
-      <div
-        onClick={() => {
-          props.setWhatsOnYourMindValue(textAreaValue);
-          props.closeCreatePost();
-        }}
-        className={`${styles.overlay} ${
-          props.popUp ? styles.overlayActive : undefined
-        }`}
-      ></div>
-    </div>
-  );
-};
+
+          <div className={styles.thirdContainer}>
+            <div>
+              <div>Add to your post</div>
+              <div>
+                <div
+                  style={{
+                    backgroundColor: props.popUpPictureAddingArea
+                      ? "black"
+                      : "",
+                  }}
+                  onClick={props.openPictureAddingArea}
+                >
+                  <img src={images} alt="" height="24px" width="24px" />
+                </div>
+                <div>
+                  <img src={tagPeople} alt="" height="24px" width="24px" />
+                </div>
+                <div>
+                  <img src={feeling} alt="" height="24px" width="24px" />
+                </div>
+                <div>
+                  <img src={checkIn} alt="" height="24px" width="24px" />
+                </div>
+                <div>
+                  <img src={lifeEvent} alt="" height="24px" width="24px" />
+                </div>
+                <div>
+                  <Options />
+                </div>
+              </div>
+            </div>
+            <div
+              className={
+                props.image || textAreaValue !== "" ? styles.blueDiv : undefined
+              }
+            >
+              <button
+                className={
+                  props.image || textAreaValue !== ""
+                    ? styles.blueButton
+                    : undefined
+                }
+                type="button"
+              >
+                Post
+              </button>
+              {/* REMINDER: the post picture method goes on this div */}
+              {props.image || textAreaValue !== "" ? (
+                <div
+                  onClick={() => {
+                    props.inputFileHandler();
+                    setTextAreaValue("");
+                    setImageUrl(null);
+                    props.closeCreatePost();
+                  }}
+                ></div>
+              ) : null}
+            </div>
+          </div>
+        </form>
+        <div
+          onClick={() => {
+            props.setWhatsOnYourMindValue(textAreaValue);
+            props.closeCreatePost();
+          }}
+          className={`${styles.overlay} ${
+            props.popUp ? styles.overlayActive : undefined
+          }`}
+        ></div>
+      </div>
+    );
+  }
+);
--- src/components/homePage/main/whatsOnYourMind/WhatsOnYourMind.tsx
+++ src/components/homePage/main/whatsOnYourMind/WhatsOnYourMind.tsx
@@ -5,88 +5,99 @@ import { DefaultProfilePicture } from "../../../../utils/svgsFunction";
 import styles from "../../../../styles/homePage/main/whatsOnYourMind/whatsOnYourMind.module.css";
 import { fetchUserImage } from "../../../../utils/fetchUserImage";
 import { fetchUserName } from "../../../../utils/fetchUserName";
-import { useEffect, useState } from "react";
+import { useEffect, useState, forwardRef } from "react";
 import { CreatePost } from "./CreatePost";
 
-export const WhatsOnYourMind = () => {
-  const [profileImageURL, setProfileImageURL] = useState<string>("");
-  const [firstName, setFirstName] = useState<string>("");
-  const [surname, setSurname] = useState<string>("");
-  const [showCreatePost, setShowCreatePost] = useState(false);
-  const [showPictureAddingArea, setShowPictureAddingArea] = useState(false);
-  const [whatsOnYourMindValue, setWhatsOnYourMindValue] = useState("");
+interface Props {
+  inputFileHandler: (e?: React.ChangeEvent<HTMLInputElement>) => Promise<void>;
+  image: File | null;
+}
 
-  useEffect(() => {
-    const userID = localStorage.getItem("UserID");
-    const getUserName = async () => await fetchUserName(userID);
-    getUserName().then((userName) => {
-      setFirstName(userName[0].firstName);
-      setSurname(userName[0].surname);
-    });
-    const getUserImage = async () => await fetchUserImage(userID);
-    getUserImage().then((imageLink) => setProfileImageURL(imageLink));
-  }, []);
+export const WhatsOnYourMind = forwardRef<HTMLTextAreaElement, Props>(
+  ({ inputFileHandler, image }, ref) => {
+    const [profileImageURL, setProfileImageURL] = useState<string>("");
+    const [firstName, setFirstName] = useState<string>("");
+    const [surname, setSurname] = useState<string>("");
+    const [showCreatePost, setShowCreatePost] = useState(false);
+    const [showPictureAddingArea, setShowPictureAddingArea] = useState(false);
+    const [whatsOnYourMindValue, setWhatsOnYourMindValue] = useState("");
 
-  const popUpCreatePost = () => setShowCreatePost(!showCreatePost);
-  const popUpPictureAddingArea = () => setShowPictureAddingArea(true);
-  const closePictureAddingArea = () => setShowPictureAddingArea(false);
+    useEffect(() => {
+      const userID = localStorage.getItem("UserID");
+      const getUserName = async () => await fetchUserName(userID);
+      getUserName().then((userName) => {
+        setFirstName(userName[0].firstName);
+        setSurname(userName[0].surname);
+      });
+      const getUserImage = async () => await fetchUserImage(userID);
+      getUserImage().then((imageLink) => setProfileImageURL(imageLink));
+    }, []);
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.whatsOnYourMind}>
-        <div className={styles.imageHolder}>
-          <DefaultProfilePicture userImage={profileImageURL} />
-          <div className={styles.overlayDiv}></div>
+    const popUpCreatePost = () => setShowCreatePost(!showCreatePost);
+    const popUpPictureAddingArea = () => setShowPictureAddingArea(true);
+    const closePictureAddingArea = () => setShowPictureAddingArea(false);
+
+    return (
+      <div className={styles.container}>
+        <div className={styles.whatsOnYourMind}>
+          <div className={styles.imageHolder}>
+            <DefaultProfilePicture userImage={profileImageURL} />
+            <div className={styles.overlayDiv}></div>
+          </div>
+          <div
+            className={
+              whatsOnYourMindValue
+                ? styles.coloredYourMindText
+                : styles.onYourMindText
+            }
+            onClick={popUpCreatePost}
+          >
+            <span>
+              {whatsOnYourMindValue ||
+                "What's on your mind, " + firstName + "?"}
+            </span>
+          </div>
         </div>
-        <div
-          className={
-            whatsOnYourMindValue
-              ? styles.coloredYourMindText
-              : styles.onYourMindText
-          }
-          onClick={popUpCreatePost}
-        >
-          <span>
-            {whatsOnYourMindValue || "What's on your mind, " + firstName + "?"}
-          </span>
+        <div className={styles.choices}>
+          <div>
+            <div>
+              <img src={Live} alt="" width="24px" height="24px" />
+            </div>
+            <span>Live video</span>
+          </div>
+          <div
+            onClick={() => {
+              popUpCreatePost();
+              popUpPictureAddingArea();
+            }}
+          >
+            <div>
+              <img src={Photos} alt="" width="24px" height="24px" />
+            </div>
+            <span>Photo/video</span>
+          </div>
+          <div>
+            <div>
+              <img src={Feeling} alt="" width="24px" height="24px" />
+            </div>
+            <span>Feeling/activity</span>
+          </div>
         </div>
+        <CreatePost
+          ref={ref}
+          image={image}
+          inputFileHandler={inputFileHandler}
+          popUp={showCreatePost}
+          closeCreatePost={popUpCreatePost}
+          firstName={firstName}
+          userProfilePicture={profileImageURL}
+          surname={surname}
+          popUpPictureAddingArea={showPictureAddingArea}
+          openPictureAddingArea={popUpPictureAddingArea}
+          closePictureAddingArea={closePictureAddingArea}
+          setWhatsOnYourMindValue={setWhatsOnYourMindValue}
+        />
       </div>
-      <div className={styles.choices}>
-        <div>
-          <div>
-            <img src={Live} alt="" width="24px" height="24px" />
-          </div>
-          <span>Live video</span>
-        </div>
-        <div
-          onClick={() => {
-            popUpCreatePost();
-            popUpPictureAddingArea();
-          }}
-        >
-          <div>
-            <img src={Photos} alt="" width="24px" height="24px" />
-          </div>
-          <span>Photo/video</span>
-        </div>
-        <div>
-          <div>
-            <img src={Feeling} alt="" width="24px" height="24px" />
-          </div>
-          <span>Feeling/activity</span>
-        </div>
-      </div>
-      <CreatePost
-        popUp={showCreatePost}
-        closeCreatePost={popUpCreatePost}
-        firstName={firstName}
-        userProfilePicture={profileImageURL}
-        surname={surname}
-        popUpPictureAddingArea={showPictureAddingArea}
-        openPictureAddingArea={popUpPictureAddingArea}
-        closePictureAddingArea={closePictureAddingArea}
-        setWhatsOnYourMindValue={setWhatsOnYourMindValue}
-      />
-    </div>
-  );
-};
+    );
+  }
+);
