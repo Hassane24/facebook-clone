d95937bb8aff341e629e16b33951b01d84351dce
Hassane <hsisane5@gmail.com>
2023-04-13 05:29:55 +0100

    now relies on onAuthStateChanged for checking authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,42 @@
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./components/loginPage/LoginPage";
 import SignupPage from "./components/signUpPage/SignupPage";
 import Home from "./components/homePage/HomePage";
+import { auth } from "./firebase/firebase";
+import { useEffect, useState } from "react";
 function App() {
-  const localUserID = localStorage.getItem("UserID");
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>();
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) setIsLoggedIn(true);
+      else setIsLoggedIn(false);
+    });
+  }, [isLoggedIn]);
   return (
     <Router>
       <Routes>
-        <Route
-          path="/"
-          element={
-            localUserID ? <Navigate to="/home" /> : <Navigate to="login" />
-          }
-        ></Route>
+        <Route path="/" element={isLoggedIn ? <Home /> : <LoginPage />}></Route>
         <Route path="login" element={<LoginPage />}></Route>
         <Route path="sign-up" element={<SignupPage />}></Route>
-        <Route path="home" element={<Home />}></Route>
+        <Route
+          path="home"
+          element={
+            isLoggedIn ? (
+              <Home />
+            ) : (
+              <h1
+                style={{
+                  color: "white",
+                  display: isLoggedIn ? "none" : "block",
+                }}
+              >
+                Not Logged In
+              </h1>
+            )
+          }
+        ></Route>
       </Routes>
     </Router>
   );
