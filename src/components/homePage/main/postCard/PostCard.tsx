508668b1a178001d2efdcc483953a751cd8adb5e
Hassane <hsisane5@gmail.com>
2023-04-16 05:59:46 +0100

    added CommentSection.tsx to it and exported the props

--- src/components/homePage/main/postCard/PostCard.tsx
+++ src/components/homePage/main/postCard/PostCard.tsx
@@ -2,8 +2,10 @@ import { InteractWithPost, reactionObject } from "./InteractWithPost";
 import { PostContent } from "./PostContent";
 import { PostInfo } from "./PostInfo";
 import styles from "../../../../styles/homePage/main/postCard/postCard.module.css";
+import { useState } from "react";
+import { CommentSection } from "./CommentSection";
 
-interface postCardProps {
+export interface postCardProps {
   reactions: reactionObject[];
   firstName: string | null;
   dateOfCreation: string;
@@ -19,6 +21,11 @@ interface postCardProps {
 }
 
 export const PostCard = (props: postCardProps) => {
+  const [showCommentSection, setShowCommentSection] = useState(false);
+
+  const revealCommentSection = () =>
+    setShowCommentSection((prevState) => !prevState);
+
   return (
     <div className={styles.postCardContainer}>
       <PostInfo
@@ -29,6 +36,7 @@ export const PostCard = (props: postCardProps) => {
       />
       <PostContent postText={props.postText} postImage={props.postImage} />
       <InteractWithPost
+        showCommentSection={revealCommentSection}
         removeReaction={props.removeReaction}
         reactions={props.reactions}
         allOfReactors={props.allOfReactors}
@@ -36,6 +44,26 @@ export const PostCard = (props: postCardProps) => {
         numberOfInteractions={props.numberOfInteractions}
         interactionHandler={props.interactionHandler}
       />
+
+      {showCommentSection && (
+        <>
+          <CommentSection
+            dateOfCreation={props.dateOfCreation}
+            pfpURL={props.pfpURL}
+            firstName={props.firstName}
+            surname={props.surname}
+            postText={props.postText}
+            postImage={props.postImage}
+            removeReaction={props.removeReaction}
+            reactions={props.reactions}
+            allOfReactors={props.allOfReactors}
+            postName={props.postName}
+            numberOfInteractions={props.numberOfInteractions}
+            interactionHandler={props.interactionHandler}
+          />
+          <div className={styles.overlay} onClick={revealCommentSection}></div>
+        </>
+      )}
     </div>
   );
 };
