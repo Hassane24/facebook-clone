61dc20778b0a30a40c60a21f344ee21904455e55
Hassane <hsisane5@gmail.com>
2023-04-19 04:24:58 +0100

    publish comment button lights up when there's input

--- src/components/homePage/main/postCard/AddComment.tsx
+++ src/components/homePage/main/postCard/AddComment.tsx
@@ -1,27 +1,21 @@
 import { DefaultProfilePicture } from "../../../../utils/svgsFunction";
 import utilityIcons from "../../../../assets/utility-icons-3.png";
 import styles from "../../../../styles/homePage/main/postCard/commentSection.module.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
+import ContentEditable, { ContentEditableEvent } from "react-contenteditable";
 
 interface Props {
   pfpURL: string | null;
 }
 
-const iconsPositions = [
-  "-413px",
-  "-539px",
-  "-449px",
-  "-592px",
-  "-718px",
-  "-664px",
-];
+const iconsPositions = ["-413px", "-539px", "-449px", "-592px", "-718px"];
 
 export const AddComment = (props: Props) => {
-  const [comment, setComment] = useState<string | null>("Write a comment...");
+  const [comment, setComment] = useState("Write a comment...");
+  const addCommentRef = useRef<HTMLDivElement>(null);
   const { pfpURL } = props;
 
-  const onBlurHandler = (e: React.ChangeEvent<HTMLDivElement>) =>
-    setComment((e.currentTarget as HTMLDivElement).textContent);
+  const onInput = (e: ContentEditableEvent) => setComment(e.target.value);
 
   const onClickHandler = () =>
     comment === "Write a comment..." ? setComment("") : null;
@@ -33,14 +27,13 @@ export const AddComment = (props: Props) => {
         <div className={styles.pfp_overlay}></div>
       </div>
       <div className={styles.write_comment}>
-        <div
-          contentEditable="true"
-          onBlur={onBlurHandler}
-          onClick={onClickHandler}
-          suppressContentEditableWarning={true}
-        >
-          {comment}
-        </div>
+        <ContentEditable
+          disabled={false}
+          innerRef={addCommentRef}
+          onChange={onInput}
+          html={comment}
+          onFocus={onClickHandler}
+        />
         <div className={styles.icons_container}>
           {iconsPositions.map((icon, index) => (
             <div key={index}>
@@ -60,6 +53,28 @@ export const AddComment = (props: Props) => {
               ></i>
             </div>
           ))}
+
+          <div
+            className={
+              comment && comment !== "Write a comment..."
+                ? styles.add_comment_button
+                : undefined
+            }
+          >
+            <i
+              style={{
+                backgroundImage: `url(${utilityIcons})`,
+                backgroundPosition: `0px -664px`,
+                backgroundSize: "26px 856px",
+                width: "16px",
+                height: "16px",
+                backgroundRepeat: "no-repeat",
+                display: "inline-block",
+                WebkitFilter:
+                  "invert(62%) sepia(98%) saturate(12%) hue-rotate(175deg) brightness(90%) contrast(96%)",
+              }}
+            ></i>
+          </div>
         </div>
       </div>
     </div>
