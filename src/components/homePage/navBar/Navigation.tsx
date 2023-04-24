fb898d702320931d2fcd8c40c2b9582c4f97da32
Hassane <hsisane5@gmail.com>
2023-04-24 18:39:23 +0100

    added helper text when hovering over nav bar links

--- src/components/homePage/navBar/Navigation.tsx
+++ src/components/homePage/navBar/Navigation.tsx
@@ -1,20 +1,62 @@
 import { Friends, Groups, Home, Gaming } from "../../../utils/svgsFunction";
 import styles from "../../../styles/homePage/navBar/navigation.module.css";
+import { useState } from "react";
 
 export const Navigation = () => {
+  const [showHomeText, setShowHomeText] = useState(false);
+  const [showFriendsText, setShowFriendsText] = useState(false);
+  const [showGroupsText, setShowGroupsText] = useState(false);
+  const [showGamingText, setShowGamingText] = useState(false);
+
+  const revealHomeText = () => setShowHomeText(true);
+  const hideHomeText = () => setShowHomeText(false);
+
+  const revealFriendsText = () => setShowFriendsText(true);
+  const hideFriendsText = () => setShowFriendsText(false);
+
+  const revealGroupsText = () => setShowGroupsText(true);
+  const hideGroupsText = () => setShowGroupsText(false);
+
+  const revealGamingText = () => setShowGamingText(true);
+  const hideGamingText = () => setShowGamingText(false);
+
   return (
     <div className={styles.navigation}>
-      <div>
+      <div onMouseEnter={revealHomeText} onMouseLeave={hideHomeText}>
         <Home />
+        <div className={`${styles.pop_up} ${showHomeText && styles.active}`}>
+          Home
+        </div>
       </div>
-      <div>
+      <div onMouseEnter={revealFriendsText} onMouseLeave={hideFriendsText}>
         <Friends />
+        <div
+          className={`${styles.pop_up} ${
+            showFriendsText ? styles.active : undefined
+          }`}
+        >
+          Friends
+        </div>
       </div>
-      <div>
+      <div onMouseEnter={revealGroupsText} onMouseLeave={hideGroupsText}>
         <Groups />
+        <div
+          className={`${styles.pop_up} ${
+            showGroupsText ? styles.active : undefined
+          }`}
+        >
+          Groups
+        </div>
       </div>
-      <div>
+      <div onMouseEnter={revealGamingText} onMouseLeave={hideGamingText}>
         <Gaming />
+        <div
+          className={`${styles.pop_up} ${
+            showGamingText ? styles.active : undefined
+          }`}
+        >
+          Gaming
+        </div>
       </div>
     </div>
   );
--- src/components/homePage/navBar/Settings.tsx
+++ src/components/homePage/navBar/Settings.tsx
@@ -22,21 +22,65 @@ const Settings = ({
   surname,
 }: SettingsProps) => {
   const [showPopUp, setShowPopUp] = useState<Boolean>(false);
+  const [showMenuText, setShowMenuText] = useState(false);
+  const [showMessengerText, setShowMessengerText] = useState(false);
+  const [showAccountText, setShowAccountText] = useState(false);
+  const [showNotificationText, setShowNotificationText] = useState(false);
+
+  const revealMenuText = () => setShowMenuText(true);
+  const hideMenuText = () => setShowMenuText(false);
+
+  const revealMessengerText = () => setShowMessengerText(true);
+  const hideMessengerText = () => setShowMessengerText(false);
+
+  const revealAccountText = () => setShowAccountText(true);
+  const hideAccountText = () => setShowAccountText(false);
+
+  const revealNotificationText = () => setShowNotificationText(true);
+  const hideNotificationText = () => setShowNotificationText(false);
 
   const onClickHandler = () => setShowPopUp(!showPopUp);
 
   return (
     <div className={styles.settings}>
-      <div>
+      <div onMouseLeave={hideMenuText} onMouseEnter={revealMenuText}>
         <Menu />
+        <div
+          className={`${styles.pop_up} ${
+            showMenuText ? styles.active : undefined
+          }`}
+        >
+          Menu
+        </div>
       </div>
-      <div>
+      <div onMouseLeave={hideMessengerText} onMouseEnter={revealMessengerText}>
         <Messenger />
+        <div
+          className={`${styles.pop_up} ${
+            showMessengerText ? styles.active : undefined
+          }`}
+        >
+          Messenger
+        </div>
       </div>
-      <div>
+      <div
+        onMouseLeave={hideNotificationText}
+        onMouseEnter={revealNotificationText}
+      >
         <Notifications />
+        <div
+          className={`${styles.pop_up} ${
+            showNotificationText ? styles.active : undefined
+          }`}
+        >
+          Notifications
+        </div>
       </div>
-      <div onClick={onClickHandler}>
+      <div
+        onMouseLeave={hideAccountText}
+        onMouseEnter={revealAccountText}
+        onClick={onClickHandler}
+      >
         <DefaultProfilePicture userImage={userImage} />
         <AccountPopUp
           profilePicture={profilePicture}
@@ -44,6 +88,14 @@ const Settings = ({
           firstName={firstName}
           surname={surname}
         />
+        <div
+          className={`${styles.pop_up} ${
+            showAccountText ? styles.active : undefined
+          }`}
+          style={{ left: "-50%" }}
+        >
+          Account
+        </div>
       </div>
     </div>
   );
