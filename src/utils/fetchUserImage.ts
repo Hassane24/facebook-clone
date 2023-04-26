15ae8b302f803b9fc006f92850b80aae53255b50
Hassane <hsisane5@gmail.com>
2023-04-26 22:26:30 +0100

    deleted useless files

--- src/utils/fetchUserImage.ts
+++
@@ -1,4 +0,0 @@
-import { ref, getDownloadURL } from "firebase/storage";
-import { storage } from "../firebase/firebase";
-export const fetchUserImage = async (userID: string | null) =>
-  await getDownloadURL(ref(storage, `profile-pics/${userID}.png`));
